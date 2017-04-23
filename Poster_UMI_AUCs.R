ff52ce45ca45c537431a0cb2aca63e9cda268737
Tallulah Andrews <ta6@sanger.ac.uk>
2017-04-23 17:15:10 +0100

    Poster2

--- Poster_UMI_AUCs.R
+++ Poster_UMI_AUCs.R
@@ -52,11 +52,28 @@ for(i in 1:length(sets)) {
 png("Figure_UMI_FS.png", width=6*1.2, height=6, units="in", res=300)
 layout(matrix(c(1,1,1,1,2,2,1,1,1,1,2,2), nrow=2, byrow=T))
 par(mar=c(3.5,3.5,1,1))
-QualityPlot(my_methods, paste(my_FS_names, "\n"), c(my_FS_colours), zoom=FALSE)
+QualityPlot(my_methods, paste("\n",my_FS_names, "\n"), c(my_FS_colours), zoom=FALSE)
 dev.off();
 
-png("Figure_UMI_FS_Top.png", width=6*1.2, height=6, units="in", res=300)
-leg_order = order(-score)
-par(las=3)
-QualityRecallPlot(my_recall[leg_order], my_FS_names[leg_order], my_FS_colours[leg_order])
-dev.off();
+### Recall/Features Plot
+xes <- seq(from=10, to=2000, by=5)
+curves <- matrix(0, ncol=length(xes), nrow=length(sets))
+for(i in 1:length(sets)) {
+	if (is.vector(sets[[i]])) {
+                t = data.frame(Gene=names(sets[[i]]), p.value=sets[[i]])
+        } else {
+                t = data.frame(Gene=rownames(sets[[i]]), p.value=sets[[i]][,1])
+        }
+	recalls <- sapply(xes, function(x) {Quality_TopRecall(t, x)})
+	curves[i,] = recalls
+}
+png("Poster_UMI_FS_TopX.png", width=5, height=5*1.2, units="in", res=300)
+par(mar=c(4,4,0,1))
+plot(1,1, col="white", xlim=c(0, max(xes)), ylim=c(0, max(curves)), xlab="Number of Features", ylab="True Positives")
+for (i in 1:length(sets)) {
+	lines(xes, curves[i,], col=my_FS_colours[i], lwd=2)
+}
+my_order = order(-curves[,length(curves[1,])])
+legend("topleft", my_FS_names[my_order], col=my_FS_colours[my_order], lty=1, lwd=2, bty="n")
+mtext("B", side=2, at=200, font=2, las=2, line=3, cex=1.75)
+dev.off()
